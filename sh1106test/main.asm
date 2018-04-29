52b74436975b2f4e94d9797a8a7b5fecb032086b
Heiko Schaefer <heiko.schaefer@swissonline.ch>
2018-04-29 16:48:38 +0200

    Updated main.asm

--- sh1106test/main.asm
+++ sh1106test/main.asm
@@ -1,12 +1,77 @@
 ;
-; sh1106test.asm
+; sh1106.asm
 ;
-; Created: 29/04/2018 16:29:13
+; Created: 30/03/2018 14:27:25
 ; Author : heiko
 ;
+; simple driver for SH1106 128x64 OLED display, ripped from sh1106 Arduino library
+
+.include "m328pdef.inc"
+
+.equ FCPU = 16000000              ; 16 Mhz arduino micro
+
+.include "i2c.inc"
+.include "sh1106.inc"
+
+; --------------------- RAM -----------------------
+.dseg
+
+.org 0x0100    ; start of the free space
 
 
-; Replace with your application code
-start:
-    inc r16
-    rjmp start
+.cseg ;------------  FLASH ----------------------------------
+.org 0x0000
+                    rjmp Reset
+
+
+.org INT_VECTORS_SIZE
+
+.include "no_timer_delays.asm"
+.include "i2c.asm"
+.include "sh1106.asm"
+                   
+; --------------------------- MAIN ------------------------------
+Reset:
+                    ;; set up return stack
+                    ldi r16, LOW(RAMEND)
+                    out SPL, r16
+                    ldi r16, HIGH(RAMEND)
+                    out SPH, r16
+                    sei
+
+
+Display:
+                    ; this is SH1106 test code
+                    rcall SH1106_init
+                    rcall SH1106_clear
+
+                    ldi r19, 8
+                    ldi r18, 16 
+                    ldi r20, 0x00  ; row 
+              b2:   ldi r21, 0x00  ; column
+                    ldi r18, 16
+              b1:   mov r16, r21
+                    mov r17, r20
+                    rcall SH1106_set_xy
+                    ldi r16, 'R'
+                    rcall SH1106_putchar
+                    rcall SH1106_show
+                    rcall Delay100ms
+
+                    inc r21
+                    dec r18   ; column iterator
+                    brne b1
+                    inc r20
+                    dec r19
+                    brne b2
+                    rjmp Fini
+                    
+
+Fini:               rjmp Fini
+
+
+; =============== SUBROUTINES =======================
+
+                   
+
+
